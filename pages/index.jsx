4275c99e1eda6bf73f7f5db1b7eb995a6aaf7627
Yuuto Harada <mryuu.dev@gmail.com>
2022-05-22 14:44:56 +0900

    'Assets, Metadata, and CSS' is finished

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,27 +1,23 @@
-import { useState } from 'react';
+import Head from 'next/head';
+import Layout, {siteTitle} from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
 
-function Header({ title }) {
-  return <h1>{title ? title : 'Default title'}</h1>;
-}
-
-export default function HomePage() {
-  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
-  const [likes, setLikes] = useState(0);
-
-  function handleClick() {
-    setLikes(likes + 1);
-  }
-
+export default function Home() {
   return (
-    <div>
-      <Header title="Develop. Preview. Ship. 🚀" />
-      <ul>
-        {names.map((name) => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
-
-      <button onClick={handleClick}>Like ({likes})</button>
-    </div>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hello, I'm <strong>Yuuto</strong>. I'm a university student at Hokkaido.{' '}
+          You can contact me on <a href='https://twitter.com/ohayouyuto'>Twitter</a>
+        </p>
+        <p>
+          (This is a sample website - you'll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
   );
 }
